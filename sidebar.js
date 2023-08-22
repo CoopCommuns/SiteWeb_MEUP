e50b606b412f7de235fa5f41afd17c77fdd3cf0a
= <=>
2023-08-22 22:15:32 +0200

    mise à jour

--- sidebar.js
+++ sidebar.js
@@ -12,17 +12,17 @@ window.addEventListener("scroll", function() {
             sidebar.style.position = "absolute";
             sidebar.style.top = "auto";
             sidebar.style.bottom = "240px";
-            sidebar.style.marginLeft = "73%";
+            sidebar.style.marginLeft = "75%";
         } else if (window.scrollY >= headerHeight) {
             sidebar.style.position = "fixed";
             sidebar.style.top = "0";
             sidebar.style.bottom = "auto";
-            sidebar.style.marginLeft = "73%";
+            sidebar.style.marginLeft = "75%";
         } else {
             sidebar.style.position = "relative";
             sidebar.style.top = "0px";
             sidebar.style.bottom = "auto";
-            sidebar.style.marginLeft = "73%";
+            sidebar.style.marginLeft = "75%";
         }
     } else {
         // Si la largeur de l'écran est inférieure à 768px, désactiver le positionnement fixe
